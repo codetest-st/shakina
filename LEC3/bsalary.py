a215e6ac8f6262c1eb2d1415ca1207988e4e58a3
codetest-st <sakinarajput83@gmail.com>
2026-07-06 02:15:47 -0700

    salary

---
+++ LEC3/bsalary.py
@@ -0,0 +1,6 @@
+salary=float(input("enter salary:"))
+hra=float(input("enter hra:"))
+ba=float(input("enter ba:"))
+pf=float(input("enter pf:"))
+total=salary+hra+ba-pf
+print(f"the net salary is:{total}")
